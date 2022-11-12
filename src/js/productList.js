bc5d530a0bf49c3d3727561d971aba8b4be297fe
hol16046 <hol16046@byui.edu>
2022-11-12 02:54:02 -0700

    Individual Assignment 3

--- src/js/productList.js
+++ src/js/productList.js
@@ -1,3 +1,5 @@
+import { renderListWithTemplate } from './utils.js';
+
 export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category;
@@ -9,19 +11,24 @@ export default class ProductList {
         this.renderList(list);
     }
     renderList(list) {
+        this.listElement.innerHTML = '';
         const template = document.getElementById('product-card-template');
-        list.forEach(product => {
-            const clone = template.contentEditable.cloneNode(true);
-            const hydratedTemplate = this.prepareTemplate(clone, product);
-            this.listElement.appendChild(hydratedTemplate);
-        })
+        this.renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
     }
     prepareTemplate(template, product) {
         template.querySelector('a').href += product.Id;
         template.querySelector('img').src = product.Image;
         template.querySelector('img').alt += product.Name;
-        template.querySelector('.card__brand').value = product.Brand.Name;
-        template.querySelector('.card__name').value = product.Name;
-        template.querySelector('.product-card__price').value += product.SuggestedRetailPrice;
+        template.querySelector('.card__brand').textContent = product.Brand.Name;
+        template.querySelector('.card__name').textContent = product.NameWithoutBrand;
+        template.querySelector('.product-card__price').textContent += product.FinalPrice;
     }
+    // renderList(list) {
+    // const template = document.getElementById('product-card-template');
+    // list.forEach(product => {
+    // const clone = template.content.cloneNode(true);
+    // const hydratedTemplate = this.prepareTemplate(clone, product);
+    // this.listElement.appendChild(hydratedTemplate);
+    // })
+    // }
 }
