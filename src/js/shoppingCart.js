43b2491d5ad717f1bd717d30ddf94d1df969b013
hol16046 <hol16046@byui.edu>
2022-11-19 01:02:21 -0700

    Update Cart Quantity

--- src/js/shoppingCart.js
+++ src/js/shoppingCart.js
@@ -1,4 +1,4 @@
-import { renderListWithTemplate, getLocalStorage } from './utils.js';
+import { renderListWithTemplate, getLocalStorage, setLocalStorage } from './utils.js';
 
 export default class CartList {
   constructor (key, listElement) {
@@ -9,10 +9,12 @@ export default class CartList {
   async init() {
     const list = getLocalStorage(this.key);
     this.renderList(list);
+    document.getElementById('minus').addEventListener('click', this.minus.bind(this));
+    document.getElementById('plus').addEventListener('click', this.plus.bind(this));
   }
   
   prepareTemplate(template, product) {
-    template.querySelector('.cart-card__image img').src =  product.Image;
+    template.querySelector('.cart-card__image img').src =  product.Images.PrimarySmall;
     template.querySelector('.cart-card__image img').alt += product.Name;
     template.querySelector('.card__name').textContent = product.Name;
     template.querySelector('.cart-card__color').textContent = product.Colors[0].ColorName;
@@ -28,5 +30,20 @@ export default class CartList {
     const template = document.getElementById('cart-card-template');
     renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
     
+  }
+
+  minus() {
+    let items = getLocalStorage('so-cart');
+    const item = items.find(item => item.Id);
+    item.Quantity != null ? item.Quantity-- : item.Quantity = 0;
+    setLocalStorage('so-cart', items);
+    this.renderList(items);
+  }
+  plus() {
+    let items = getLocalStorage('so-cart');
+    const item = items.find(item => item.Id);
+    item.Quantity != null ? item.Quantity++ : item.Quantity = 2;
+    setLocalStorage('so-cart', items);
+    this.renderList(items);
   }
 }
