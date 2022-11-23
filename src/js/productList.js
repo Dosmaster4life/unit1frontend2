cbad5c9be2606e1a7f6a788ad8eb1da09e7f4efc
dosmaster4life <jefferysuperman@gmail.com>
2022-11-22 19:08:50 -0700

    model

--- src/js/productList.js
+++ src/js/productList.js
@@ -14,7 +14,56 @@ export default class ProductList {
   }
   
   prepareTemplate(template, product) {
-    console.log(product)
+    // add a floating button to the template
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-primary', 'btn-sm', 'float-right');
+    button.innerHTML = 'Quick View';
+    // on click of the button, show the modal
+    button.addEventListener('click', () => {
+      // create a modal element that pops up when the button is clicked 
+      const modal = document.createElement('div');
+      // set classname to modal
+    
+      modal.classList.add('modal', 'fade');
+      modal.setAttribute('tabindex', '-1');
+      modal.setAttribute('role', 'dialog');
+      // set modal visible
+      modal.style.display = 'block';
+      console.log(product)
+   
+      modal.innerHTML = `
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">${product.Name}</h5>
+            </div>
+            <div class="modal-body">
+            <p class="product__color">${product.Colors[0].ColorName}</p>
+            <p class="product-card__price">$<s>${product.FinalPrice}</s> $${(product.FinalPrice - (product.FinalPrice * .15)).toFixed(2)}<span class="discount">15% Off Today Only!</span></p>
+              <p>${product.DescriptionHtmlSimple}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      `;
+      // event listener for the modal to close when the close button is clicked
+      modal.querySelector('.btn-secondary').addEventListener('click', () => {
+        modal.style.display = 'none';
+      });
+      // show the modal for 3 seconds
+      document.body.appendChild(modal);
+    
+   
+
+    
+      
+    });
+
+  
+
+   
     template.querySelector('a').href +=  product.Id;
     template.querySelector('img').src = product.Images.PrimaryMedium ;
     template.querySelector('img').alt += product.Name;
@@ -42,12 +91,14 @@ export default class ProductList {
     // add strikeout price to price element
     price.appendChild(strikeoutPrice);
     price.appendChild(discountPrice);
+    template.querySelector('.product-card').appendChild(button);
     // remove $ from .product-card__price
     template.querySelector('.product-card__price').textContent = '';
     template.querySelector('.product-card__price').appendChild(price);
     // add discount price to price element
     // add 15% Off Today Only
     template.querySelector('.product-card__price').appendChild(discountText);
+
    
 
     template.querySelector
