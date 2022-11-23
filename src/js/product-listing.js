2d97d70b0ddad1999687d06a000f26f540ad92ff
dosmaster4life <jefferysuperman@gmail.com>
2022-11-22 19:08:41 -0700

    model for quick view

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,9 +4,12 @@ import { loadHeaderFooter, getParam } from './utils.js';
 
 loadHeaderFooter();
 
+
 const category = getParam('category');
 const dataSource = new ExternalServices();
 const listElement = document.querySelector('.product-list');
 const product_list = new ProductList(category, dataSource, listElement);
+
+
 
 product_list.init();
